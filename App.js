0c79829f0af3d0351dd515905e5e11e32e9c4a23
Dheph <dhepherson.miranda@outlook.com>
2020-12-12 10:47:19 -0300

    feature: delete note

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   TextInput,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import { SwipeListView } from "react-native-swipe-list-view";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -17,19 +18,27 @@ const App = () => {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
 
-  console.log(notes.length);
   function addNote() {
     setOpen(true);
   }
   function createNote() {
     let note = {
+      id:Math.floor(Math.random() * 1000),
       title: title,
       text: text,
     };
+    console.log(note.id)
     setNotes((notes) => [...notes, note]);
-    setTitle(""); 
-    setText(""); 
+    setTitle("");
+    setText("");
     setOpen(false);
+  }
+
+  function deleteNote(indexNote){
+    const newData = [...notes];
+        const prevIndex = notes.findIndex(item => item.id === indexNote);
+        newData.splice(prevIndex, 1);
+        setNotes(newData);
   }
 
   return (
@@ -39,32 +48,48 @@ const App = () => {
       </View>
 
       <View style={styles.contentList}>
-        <FlatList
+        <SwipeListView
           data={notes}
+          rightOpenValue={-150}
+          previewRowKey={"0"}
+          previewOpenValue={-40}
+          previewOpenDelay={3000}
           showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <View style={styles.note}>
+          renderHiddenItem={(hidden) => (
+            
+            <View style={styles.hiddenButtons}>
+              <TouchableOpacity
+                style={[styles.backRightBtn, styles.backRightBtnRight]}
+                onPress={() => deleteNote(hidden.item.id)}
+              >
+                <Feather name="trash" size={25} color="#fff"/>
+              </TouchableOpacity>
+            </View>
+          )}
+          renderItem={({item}) => (
+            <View style={styles.note} onPress={() => console.log(item.id)}>
               <Text style={styles.noteTitle}> {item.title}</Text>
               <Text style={styles.noteText}> {item.text}</Text>
             </View>
           )}
+          
         />
       </View>
       <View style={styles.footer}>
-          <TouchableOpacity
-              style={styles.button}
-              onPress={() => addNote()}
-            >
-              <Feather name="plus" size={20} color="#fff" />
-            </TouchableOpacity>
-          </View>
+        <TouchableOpacity style={styles.button} onPress={() => addNote()}>
+          <Feather name="plus" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
       <StatusBar barStyle="light-content" />
       {
         <Modal visible={open} animationType="slide">
           <View style={{ ...styles.container, backgroundColor: "#313866" }}>
-            <View style={{ ...styles.header, backgroundColor: "#1B1B1F" }}>
-              <Text style={styles.title}> D-Do </Text>
-            </View>
+            <TouchableOpacity
+              style={{ ...styles.header, backgroundColor: "#1B1B1F" }}
+              onPress={() => setOpen(false)}
+            >
+              <Text style={styles.title}> Go back </Text>
+            </TouchableOpacity>
 
             <View style={styles.contentModal}>
               <View style={styles.form}>
@@ -89,14 +114,14 @@ const App = () => {
                 />
               </View>
             </View>
-          <View style={styles.footer}>
-          <TouchableOpacity
-              style={{ ...styles.button, backgroundColor: "#1B1B1F" }}
-              onPress={() => createNote()}
-            >
-              <Feather name="plus" size={20} color="#fff" />
-            </TouchableOpacity>
-          </View>
+            <View style={styles.footer}>
+              <TouchableOpacity
+                style={{ ...styles.button, backgroundColor: "#1B1B1F" }}
+                onPress={() => createNote()}
+              >
+                <Feather name="plus" size={20} color="#fff" />
+              </TouchableOpacity>
+            </View>
           </View>
         </Modal>
       }
@@ -125,10 +150,10 @@ const styles = StyleSheet.create({
   contentList: {
     height: "80%",
   },
-  contentModal:{
-    alignItems:'center',
-    justifyContent:"center",
-    height:'80%',
+  contentModal: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "80%",
   },
   note: {
     borderRadius: 20,
@@ -146,36 +171,56 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "400",
     color: "#fff",
-    margin:'2%'
+    margin: "2%",
   },
-  inputTitle:{
-    fontSize:40,
-    color:'#fff'
+  inputTitle: {
+    fontSize: 40,
+    color: "#fff",
   },
-  inputText:{
-    fontSize:18,
-    color:'#fff'
+  inputText: {
+    fontSize: 18,
+    color: "#fff",
   },
-  footer:{
-    justifyContent:'center',
-    alignItems:'center'
+  footer: {
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
     borderRadius: 50,
-    width:60,
-    height:60,
-    justifyContent:"center",
-    alignItems:'center',
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: "#50409A",
-    margin:"2%"
+    margin: "2%",
   },
   textAreaContainer: {
-    padding: "10%",
+    padding: "1%",
   },
   textArea: {
     height: "50%",
     justifyContent: "flex-start",
     color: "#fff",
   },
+  hiddenButtons:{
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+  },
+  backRightBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    width: 75,
+    padding:10,
+    borderRadius:20,
+    
+},
+backRightBtnRight: {
+    backgroundColor: "#313866",
+    right: '5%',
+},
 });
 export default App;
